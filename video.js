7c8671bfe8e9b06f7d5e44ce19bed0c8f701f37c
fapjsc <p82zsh@gmail.com>
2022-04-13 11:37:59 +0800

    change url

--- video.js
+++ video.js
@@ -7,7 +7,7 @@ let command = null;
 
 //**  PI  */
 command = ffmpeg()
-  .input("/home/pi/Downloads/jackpot.mp4")
+  .input("/home/pi/Downloads/chrome.webm")
   .inputOptions("-re")
   .inputOption("-stream_loop -1")
   .addOptions(["-vcodec libx264", "-acodec aac"])
