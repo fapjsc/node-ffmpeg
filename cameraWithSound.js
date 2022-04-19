8679b3fbed4de135f866a2218b749111e5fc8355
fapjsc <p82zsh@gmail.com>
2022-04-19 14:28:43 +0800

    add camera with sound

---
+++ cameraWithSound.js
@@ -0,0 +1,49 @@
+const ffmpeg = require("fluent-ffmpeg");
+const colors = require("colors");
+const { getIPAddress } = require("./helpers");
+
+const ip = getIPAddress();
+
+const outputPath = `rtmp://220.135.67.240/game/${ip.split(".").pop()}`;
+
+
+let command = null;
+
+//**  PI  */
+command = ffmpeg()
+  .input("/dev/video0")
+  .inputOptions("-s 800x600")
+  .addInput("plughw:2")
+  .addOptions([
+    "-c:v libx264",
+    "-profile:v main",
+    "-pix_fmt yuv420p",
+    "-g 5",
+    "-b:v 700k",
+    "-movflags faststart",
+    "-tune zerolatency",
+    "-preset ultrafast",
+    "-vf scale=800:750",
+    "-bf 0"
+  ])
+  .format("flv")
+  .output(outputPath)
+  .inputOption("-f alsa")
+  .addOptions(["-c:a aac", "-b:a 64k"])
+
+  //** LISTEN */
+  .on("start", function (commandLine) {
+    console.log(`[  ${new Date()}  ] Vedio is Pushing To ${outputPath} !`.green);
+    console.log(`commandLine:  + ${commandLine}`.blue);
+  })
+  .on("error", function (err, stdout, stderr) {
+    console.log(`error:   + ${err.message}`.red.inverse);
+  })
+  .on("end", function () {
+    console.log(
+      `[  ${new Date()}  ] Vedio Pushing is Finished !`.yellow.inverse
+    );
+  })
+ 
+
+command.run();
